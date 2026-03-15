20ad5eaaca1b3deea0e1feca44772a1ad7c28d65
Ch-SaiTeja333 <chintakrindasaiteja@gmail.com>
2026-03-15 11:33:39 +0530

    update frontend url in the server cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = 8080;
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://skillcheck-ai-project-groq-1.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 app.use("/user-api", userRoutes);
