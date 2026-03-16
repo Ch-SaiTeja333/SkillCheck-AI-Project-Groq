ffbc0f4a64802aabd05580018497982dae348ed7
Ch-SaiTeja333 <chintakrindasaiteja@gmail.com>
2026-03-16 11:19:37 +0530

    fix refresh bug-1

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
    React Router Fix (IMPORTANT)
 ================================ */
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
